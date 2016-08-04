7d2b7c965568e77a4ff049bde5a4540cb019c8d1
ArnieTheOne <ArnieTheOne@users.noreply.github.com>
2016-08-04 16:35:54 +0200

    downgrade to last stable version

--- Agario Script.js
+++ Agario Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name		The ULTIMATE Agario Script [v2.2]
-// @name:en		The ULTIMATE Agario Script [v2.2]
-// @name:de		The ULTIMATE Agario Script [v2.2]
+// @name		The ULTIMATE Agario Script
+// @name:en		The ULTIMATE Agario Script
+// @name:de		The ULTIMATE Agario Script
 // @namespace		http://tampermonkey.net/
 // @version		2.2
 // @description		Easily configurable keys in code. Credits to Jack Burch, Tom Burris AND Ali Ahfad Mehdi
@@ -13,6 +13,7 @@
 // @run-at		document-end
 // @require		https://greasyfork.org/scripts/21918-color-changer/code/color%20changer.js?version=139529
 // ==/UserScript==
+
 window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
 var Feed = false;
@@ -165,77 +166,16 @@ new agar.io style
 syntax:
 changeColor("font", "bgColor", "id", "class")
 */
-/*
+
 $('hr').css({'width':'0px','height':'0px'});
 changeColor("white", "rgba(0, 0, 0, 0.5)", "", "agario-panel");
-changeColor("rgba(0, 0, 0, 1)", "white", "", "agario-wallet-container")
-*/
+changeColor("rgba(0, 0, 0, 1)", "white", "", "agario-wallet-container");
+$('.text-muted')[1].style.color = 'white';
+$('span').css({'color':'white'});
+
 //'hide' advertisement
 $('#advertisement').css({'position':'absolute','left':'-100%'})
 
-$('#settings').css({'height':'220px'})
-
-// =========================options=================================
-var UASoptions = document.createElement('left');
-UASoptions.name = 'UASoptions';
-UASoptions.id = 'UASoptions';
-var UASform = document.createElement('form');
-UASform.name = 'selectTheme';
-var darkLabel = document.createElement('label');
-var extremeDarkLabel = document.createElement('label');
-var defaultLabel = document.createElement('label');
-var darkRadio = document.createElement('input');
-var extremeDarkRadio = document.createElement('input');
-var defaultRadio = document.createElement('input');
-var darkSpan = document.createElement('span');
-var extremeDarkSpan = document.createElement('span');
-var defaultSpan = document.createElement('span');
-
-darkLabel.name = 'darkLabel';
-darkLabel.id = 'darkLabel';
-extremeDarkLabel.name = 'extremeDarkLabel';
-extremeDarkLabel.id = 'extremeDatkLabel';
-defaultLabel.name = 'defaultLabel';
-defaultLabel.id = 'defaultLabel';
-darkRadio.type = 'radio';
-//darkRadio.name = 'dark';
-darkRadio.name = 'theme';
-darkRadio.value = 'dark';
-extremeDarkRadio.type = 'radio';
-//extremeDarkRadio.name = 'extremeDark';
-extremeDarkRadio.name = 'theme';
-extremeDarkRadio.value = 'extremeDark';
-defaultRadio.type = 'radio';
-//defaultRadio.name = 'default';
-defaultRadio.name = 'theme';
-defaultRadio.value = 'default';
-darkSpan.innerHTML = 'dark theme';
-extremeDarkSpan.innerHTML = 'extreme Dark theme';
-defaultSpan.innerHTML = 'default theme';
-
-
-
-//darkLabel.appendChild(darkRadio);
-//darkLabel.appendChild(darkSpan);
-//extremeDarkLabel.appendChild(extremeDarkRadio);
-//extremeDarkLabel.appendChild(extremeDarkSpan);
-//defaultLabel.appendChild(defaultRadio);
-//defaultLabel.appendChild(defaultSpan);
-
-UASform.appendChild(/*darkLabel*/darkRadio);
-UASform.appendChild(darkSpan)
-UASform.appendChild(document.createElement('br'));
-UASform.appendChild(/*extremeDarkLabel*/extremeDarkRadio);
-UASform.appendChild(extremeDarkSpan)
-UASform.appendChild(document.createElement('br'));
-UASform.appendChild(/*defaultLabel*/defaultRadio);
-UASform.appendChild(defaultSpan)
-UASform.appendChild(document.createElement('br'));
-UASoptions.appendChild(UASform);
-$('#options').append(UASoptions);
-$('#UASoptions').css({'position':'relative','left':'-25%','top':'10px'});
-
-// ================================================================
 /*
 Script by Ali Ahfad Mehdi and Arnie
 
