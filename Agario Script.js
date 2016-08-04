ce07b7055725a64fb1cb8415fdb5bee957b9bcd7
ArnieTheOne <ArnieTheOne@users.noreply.github.com>
2016-08-04 16:43:42 +0200

    downgrade to the last stable version

--- Agario Script.js
+++ Agario Script.js
@@ -3,7 +3,7 @@
 // @name:en		The ULTIMATE Agario Script
 // @name:de		The ULTIMATE Agario Script
 // @namespace		http://tampermonkey.net/
-// @version		2.2
+// @version		2.21
 // @description		Easily configurable keys in code. Credits to Jack Burch, Tom Burris AND Ali Ahfad Mehdi
 // @description:de	Easily configurable keys in code. Credits to Jack Burch, Tom Burris AND Ali Ahfad Mehdi
 // @author		Arnie
@@ -17,7 +17,7 @@
 window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
 var Feed = false;
-var Speed = 10; //default = 25
+var Speed = 10;
 var splits = 1;
 
 //Funtions
@@ -110,8 +110,8 @@ exInstructions.id = 'exInstructions';
 exInstructionsAlign.appendChild(exInstructions);
 instructions.appendChild(exInstructionsAlign);
 
-$('#exInstructions').css({'color':'#fff','cursor':'default'});
-$('#exInstructionsAlign').css({'color':'#fff','cursor':'default'});
+$('#exInstructions').css({'color':'#777','cursor':'default'});
+$('#exInstructionsAlign').css({'color':'#777','cursor':'default'});
 $('#statsContinue').css({'width':'146px'});
 $('#canvas').css({'cursor':'crosshair'})
 
@@ -166,7 +166,7 @@ new agar.io style
 syntax:
 changeColor("font", "bgColor", "id", "class")
 */
-
+/*
 $('hr').css({'width':'0px','height':'0px'});
 changeColor("white", "rgba(0, 0, 0, 0.5)", "", "agario-panel");
 changeColor("rgba(0, 0, 0, 1)", "white", "", "agario-wallet-container");
@@ -175,7 +175,7 @@ $('span').css({'color':'white'});
 
 //'hide' advertisement
 $('#advertisement').css({'position':'absolute','left':'-100%'})
-
+*/
 /*
 Script by Ali Ahfad Mehdi and Arnie
 
